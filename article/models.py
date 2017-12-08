bac25594ee3b77afe203e01389ca5799f15b9978
Sammion <samqian163@163.com>
2017-12-08 20:12:07 +0800

    增加文章发布功能

--- article/models.py
+++ article/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+from django.utils import timezone
+from django.utils.text import slugify
 
 
 # Create your models here.
@@ -11,3 +14,30 @@ class ArticleColumn(models.Model):
 
     def __str__(self):
         return self.column
+
+
+class ArticlePost(models.Model):
+    author = models.ForeignKey(User, related_name="article")
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=500)
+    column = models.ForeignKey(ArticleColumn, related_name='article_column')
+    body = models.TextField()
+    created = models.DateTimeField(default=timezone.now())
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ("title",)
+        # 创建索引
+        index_together = (('id', 'slug'),)
+
+    def __str__(self):
+        return self.title
+
+    # 重写保存方法
+    def save(self, *args, **kargs):
+        self.slug = slugify(self.title)
+        super(ArticlePost, self).save(*args, **kargs)
+
+    # 获取某个文章的url
+    def get_absolute_url(self):
+        return reverse("article:article_detail", args=[self.id, self.slug])
