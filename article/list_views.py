4165b5097c015a9617fdd4fe292648c099496091
Sammion <samqian163@163.com>
2017-12-13 20:25:49 +0800

    优化网站首页及各种页面跳转功能

--- article/list_views.py
+++ article/list_views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import ArticleColumn, ArticlePost
+from django.contrib.auth.models import User
 
-def article_titles(request):
-    articles_title = ArticlePost.objects.all()
-    paginator = Paginator(articles_title,2)
+
+def article_titles(request, username=None):
+    if username:
+        user = User.objects.get(username=username)
+        articles_title = ArticlePost.objects.filter(author=user)
+        try:
+            userinfo = user.userinfo
+        except:
+            userinfo = None
+    else:
+        articles_title = ArticlePost.objects.all()
+
+    paginator = Paginator(articles_title, 2)
     page = request.GET.get('page')
     try:
         current_page = paginator.page(page)
@@ -15,4 +26,9 @@ def article_titles(request):
     except EmptyPage:
         current_page = paginator.page(paginator.num_pages)
         articles = current_page.object_list
-    return render(request, "article/list/article_titles.html", {"articles": articles, "page": current_page})
+    if username:
+        return render(request, "article/list/article_titles.html",
+                      {"articles": articles, "page": current_page, "user": user, "userinfo": userinfo})
+    else:
+        return render(request, "article/list/article_titles.html",
+                      {"articles": articles, "page": current_page})
