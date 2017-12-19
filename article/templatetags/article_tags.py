68afd6fcba4aa83da7e8c1b029afb6f7ff531454
Sammion <samqian163@163.com>
2017-12-19 21:36:24 +0800

    使用templatetags实现最新文章的数据展示；增加最多评论文章展示

--- article/templatetags/article_tags.py
+++ article/templatetags/article_tags.py
@@ -5,7 +5,7 @@ from django import template
 register = template.Library()
 
 from article.models import ArticlePost
-
+from django.db.models import Count
 
 @register.simple_tag
 def total_articles():
@@ -22,3 +22,9 @@ def author_total_articles(user):
 def lastest_articles(n=5):
     lastest_articles = ArticlePost.objects.order_by('-created')[:n]
     return {"lastest_articles": lastest_articles}
+
+
+@register.assignment_tag
+def most_commented_articles(n=3):
+    #annotate函数对各个文章对象标注，用comments的总数来标注
+    return ArticlePost.objects.annotate(total_comments=Count('comments')).order_by("-total_comments")[:n]
